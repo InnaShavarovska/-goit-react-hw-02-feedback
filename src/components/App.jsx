59ef1e24c7e1edb74e637dd64fe3307feb56b159
InnaShavarovska <innashavarovska@gmail.com>
2023-05-04 19:59:24 +0300

    +feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Section from './Section/Section';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
+import Notific from './Notific/Notific';
 import css from './App.module.css';
 
 export class App extends Component {
@@ -11,10 +12,17 @@ export class App extends Component {
     bad: 0,
   };
 
-  onLeaveFeedback = state => {
-    this.setState(prevState => ({
-      [state]: prevState[state] + 1,
-    }));
+  onLeaveFeedback = event => {
+    this.setState({ [event]: this.state[event] + 1 });
+  };
+
+  countTotalFeedback = event => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = event => {
+    const { good } = this.state;
+    return Math.round((good / this.countTotalFeedback()) * 100);
   };
 
   render() {
@@ -30,7 +38,17 @@ export class App extends Component {
           />
         </Section>
         <Section title="Statistics">
-          <Statistics good={good} neutral={neutral} bad={bad} />
+          {this.countTotalFeedback() > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notific message="There is no feedback" />
+          )}
         </Section>
       </div>
     );
