ca3d49ac164eb0b402732d944bbdc41983a1eb9b
InnaShavarovska <innashavarovska@gmail.com>
2023-05-04 15:49:47 +0300

    +statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Section from './Section/Section';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+import css from './App.module.css';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = state => {
+    this.setState(prevState => ({
+      [state]: prevState[state] + 1,
+    }));
+  };
+
+  render() {
+    const options = Object.keys(this.state);
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <div className={css.container}>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          <Statistics good={good} neutral={neutral} bad={bad} />
+        </Section>
+      </div>
+    );
+  }
+}
